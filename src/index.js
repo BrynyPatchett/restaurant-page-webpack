fdad70a0251beb3b9d4168fef8d3052499cb9048
Bryny <28590441+BrynyPatchett@users.noreply.github.com>
2023-09-15 11:57:50 +1200

    removed static nav from index.html moved to own JS aswell as style sheet

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
+import navbar from './navbar.js';
 import home from './landing.js';
 import './style.css';
 
+const body = document.querySelector("body");
 const contentDisplay = document.querySelector('#content');
 
-
+//initalize Page
 (function (){
+     body.insertBefore(navbar(),body.firstChild)
     contentDisplay.appendChild(home());
 })();
 
---
+++ src/navbar.js
@@ -0,0 +1,24 @@
+ import './navbar-style.css';
+ 
+ export default function(){
+    let navbar = document.createElement('nav');
+    navbar.classList.add("navbar");
+
+    let navOne = document.createElement('div');
+    navOne.classList.add("navitem");
+    let navTwo = document.createElement('div');   
+    navTwo.classList.add("navitem"); 
+    let navThree = document.createElement('div');
+    navThree.classList.add("navitem");
+
+    navOne.textContent = "Home";
+    navTwo.textContent = "Menu";
+    navThree.textContent = "Contact";
+
+    navbar.appendChild(navOne);
+    navbar.appendChild(navTwo);
+    navbar.appendChild(navThree);
+
+    return navbar;
+
+ }
